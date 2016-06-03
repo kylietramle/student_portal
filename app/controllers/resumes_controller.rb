9374944506459b115e57a3052eb1b344664fb61d
Kylie Le <kylie.tram.le@gmail.com>
2016-06-02 20:38:36 -0700

    added index action and view

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,2 +1,5 @@
 class ResumesController < ApplicationController
+  def index
+    @resumes = Unirest.get("#{ENV['API']}.json", headers:{"Accept" => "application/json"}).body
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'students#index'
+  get '/students' => 'students#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
